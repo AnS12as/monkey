47e5b83ee463394f969e9a79795fe040310ac21f
Anastasiya Samokhina <anastasiya0625@gmail.com>
2024-01-17 22:51:32 +0300

    Добавлена функция для преобразования строки в верхний регистр

---
+++ str_func.py
@@ -0,0 +1,12 @@
+def convert_to_uppercase(input_string):
+    if __name__ == '__main__':
+        """Преобразует строку в верхний регистр.
+            
+            Parameters:
+            input_string (str): Входная строка
+            
+            Returs:
+            str: Строка в верхнем регистре.
+            """
+        return input_string.upper()
+
